c63488f5e044197d10542152046b1cc76e01263b
GAMIS65 <ivicko05@gmail.com>
2023-09-25 18:27:26 +0200

    Set the new language json schema as default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@
 
 import styles from './page.module.css'
 import { useEffect, useState } from 'react';
-import english from './languages/english-ansi-old.json'
+import english from './languages/english-ansi.json'
 
 type KeyProps = {
   name: string,
   isActive: boolean | string
 }
 
-const layout = english; 
+const layout = english.layout; 
 
 function Key({ name, isActive }: KeyProps) {
-  return <div className={`${styles.key} ${isActive ? styles.active : ""}`}>{name}</div>;
+  return <div className={`${styles.key} ${isActive ? styles.active : ''}`}>{name}</div>;
 }
 
 export default function Home() {
-  const [text, setText] = useState("Test 123");
+  const [text, setText] = useState('Test 123');
   const [currentLetter, setCurrentLetter] = useState(0);
   const [leftShift, setLeftShift] = useState(false);
   const [rightShift, setRightShift] = useState(false);
@@ -26,28 +26,28 @@ export default function Home() {
   };
 
   useEffect(() => {
-  if (
-    text &&
-    text[currentLetter] &&
-    (text[currentLetter] === text[currentLetter].toUpperCase() &&
-      text[currentLetter] !== text[currentLetter].toLowerCase())
-  ) {
-    setLeftShift(true);
-  } else {
-    setLeftShift(false);
-  }
-}, [text, currentLetter]);
+    if (
+      text &&
+      text[currentLetter] &&
+      (text[currentLetter] === text[currentLetter].toUpperCase() &&
+        text[currentLetter] !== text[currentLetter].toLowerCase())
+    ) {
+      setLeftShift(true);
+    } else {
+      setLeftShift(false);
+    }
+  }, [text, currentLetter]);
 
   const alphabetKeys = Object.entries(layout).map(([rowKey, row]) => (
     <div key={rowKey} className={styles.row}>
-      {Object.entries(row).map(([key, letters]) => (
+      {Object.entries(row).map(([key, keyProps]) => (
         <Key
           key={key}
-          name={letters[0] || ""}
+          name={keyProps.letter[0] || ''}
           isActive={
-            text[currentLetter] === letters[1] ||
-            (letters[2] && text[currentLetter] === letters[2]) ||
-            (letters[1] === "ShiftLeft" && leftShift)
+            text[currentLetter] === keyProps.letter[1] ||
+            (keyProps.letter[2] && text[currentLetter] === keyProps.letter[2]) ||
+            (keyProps.letter[1] === 'ShiftLeft' && leftShift)
           }
         />
       ))}
