fab8be479451f940b18e31d075c243b283ff4edd
unknown <ivicko05@gmail.com>
2023-09-26 09:27:24 +0200

    Change key color based on the finger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,18 @@ import english from './languages/english-ansi.json'
 
 type KeyProps = {
   name: string,
-  isActive: boolean | string
+  isActive: boolean | string,
+  style: string
 }
 
 const layout = english.layout; 
 
-function Key({ name, isActive }: KeyProps) {
-  return <div className={`${styles.key} ${isActive ? styles.active : ''}`}>{name}</div>;
+function Key({ name, isActive, style }: KeyProps) {
+  return <div className={`${styles.key} ${isActive ? styles[style] : ''}`}>{name}</div>;
 }
 
 export default function Home() {
-  const [text, setText] = useState('Test 123');
+  const [text, setText] = useState('A*Test 123');
   const [currentLetter, setCurrentLetter] = useState(0);
   const [leftShift, setLeftShift] = useState(false);
   const [rightShift, setRightShift] = useState(false);
@@ -26,6 +27,7 @@ export default function Home() {
   };
 
   useEffect(() => {
+    console.log(text[currentLetter])
     if (
       text &&
       text[currentLetter] &&
@@ -38,16 +40,17 @@ export default function Home() {
     }
   }, [text, currentLetter]);
 
-  const alphabetKeys = Object.entries(layout).map(([rowKey, row]) => (
+  const keyboard = Object.entries(layout).map(([rowKey, row]) => (
     <div key={rowKey} className={styles.row}>
       {Object.entries(row).map(([key, keyProps]) => (
         <Key
           key={key}
           name={keyProps.letter[0] || ''}
+          style={keyProps.finger}
           isActive={
             text[currentLetter] === keyProps.letter[1] ||
             (keyProps.letter[2] && text[currentLetter] === keyProps.letter[2]) ||
-            (keyProps.letter[1] === 'ShiftLeft' && leftShift)
+            (keyProps.letter[1] === 'ShiftLeft' && leftShift || keyProps.letter[1] === "ShiftRight" && rightShift) 
           }
         />
       ))}
@@ -58,7 +61,7 @@ export default function Home() {
     <div className={styles.wrapper}>
       <div>{text}</div>
       <input onChange={handleKeyDown} />
-      {alphabetKeys}
+      {keyboard}
     </div>
   );
 }
