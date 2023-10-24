c1d66e659d95cf790d2d1e3336c07439238b1e2b
unknown <ivicko05@gmail.com>
2023-10-24 08:11:41 +0200

    Improve letter display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,14 +34,23 @@ function LetterDisplay({ text, index }: LetterDisplayProps) {
 
   const letters = text.split("");
   console.log(text);
-
   return (
-    <div className={styles["letter-display"]}>
-      {letters.map((letter, test) => (
-        <DisplayKey key={test}  name={letter} isActive={index === test}/>
-      ))}
-    </div>
+  <div className={styles["letter-display"]}>
+
+      {text[index-1] && <DisplayKey name={text[index-1]} isActive={false}/> }
+
+      {text[index] && <DisplayKey name={text[index]} isActive={true}/>}
+      
+      {text[index+1] && <DisplayKey name={text[index+1]} isActive={false}/>}
+  </div>
   );
+  // return (
+  //   <div className={styles["letter-display"]}>
+  //     {letters.map((letter, test) => (
+  //       <DisplayKey key={test}  name={letter} isActive={index === test}/>
+  //     ))}
+  //   </div>
+  // );
   // return (
   //   <div className={styles["letter"]}>{text}</div>
   // )
