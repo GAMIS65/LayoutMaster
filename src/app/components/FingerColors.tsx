75e70c50683a23d6d6c51975cc806fc31a368a8b
unknown <ivicko05@gmail.com>
2023-12-22 12:01:34 +0100

    fix finger display css

--- src/app/components/FingerColors.tsx
+++ src/app/components/FingerColors.tsx
@@ -4,12 +4,15 @@ import styles from '../page.module.css'
 function FingerColors() {
     return (
         <div>
-            <div>
-                <div className={`${styles.square} ${styles.key} ${styles["index-right"]}`}>
+            <div className={`${styles["finger-container"]}`}>
+                <div className={`${styles.square} ${styles.key} ${styles["pinky-left"]}`}>
                 </div>
                 <p>Ľavý malíček</p>
             </div>
-            <div></div>
+            <div className={`${styles["finger-container"]}`}>
+                <div className={`${styles.square} ${styles.key} ${styles["ring-left"]}`}>
+                </div>
+                <p>Ľavý prsteník</p></div>
             <div></div>
             <div></div>
             <div></div>
