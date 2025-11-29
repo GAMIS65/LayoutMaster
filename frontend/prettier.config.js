b8591b773d4beef39847ebfb8c6991e3e24c1119
GAMIS65 <ivicko05@gmail.com>
2025-11-29 20:28:15 +0100

    init v3

---
+++ frontend/prettier.config.js
@@ -0,0 +1,13 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+};
+
+export default config;
--- frontend/src/components/CloseButton/CloseButton.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './CloseButton.module.css';
-function CloseButton() {
-    return (
-        <button className={styles.close}>X</button>
-    )
-}
-
-export default CloseButton
