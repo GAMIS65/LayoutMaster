634b20017023fb8f4ebd609f838ffcdae6bbe9c6
GAMIS65 <ivicko05@gmail.com>
2025-11-30 21:07:08 +0100

    refactor: move logo component to its own file

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import FeatureCard from '@/components/FeatureCard';
+import Logo from '@/components/Logo';
 import { createFileRoute } from '@tanstack/react-router';
 import { BarChart2, Keyboard, Heart } from 'lucide-react';
 
@@ -45,36 +46,6 @@ function App() {
           />
         </div>
       </div>
-    </div>
-  );
-}
-
-function Logo() {
-  const logoChars = [
-    { char: 'L', color: 'text-yellow-300' },
-    { char: 'a', color: 'text-red-300' },
-    { char: 'y', color: 'text-purple-300' },
-    { char: 'o', color: 'text-yellow-300' },
-    { char: 'u', color: 'text-purple-300' },
-    { char: 't', color: 'text-green-300' },
-    { char: 'M', color: 'text-purple-300' },
-    { char: 'a', color: 'text-red-300' },
-    { char: 's', color: 'text-orange-300' },
-    { char: 't', color: 'text-green-300' },
-    { char: 'e', color: 'text-blue-300' },
-    { char: 'r', color: 'text-green-300' },
-  ];
-
-  return (
-    <div className="text-shadow-sm">
-      {logoChars.map((item, index) => (
-        <span
-          key={index}
-          className={`${item.color} inline-block transition-transform duration-150 ease-out hover:-translate-y-1`}
-        >
-          {item.char}
-        </span>
-      ))}
     </div>
   );
 }
